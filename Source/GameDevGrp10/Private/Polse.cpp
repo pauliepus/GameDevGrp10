67a3cb5dfe23a451cb2d8bbc97a55923667990f4
pauliepus <269426@stud.inn.no>
2024-03-13 15:14:45 +0100

    Added Initial files for Pols

---
+++ Source/GameDevGrp10/Private/Polse.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Polse.h"
+
+// Sets default values
+APolse::APolse()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APolse::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APolse::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
