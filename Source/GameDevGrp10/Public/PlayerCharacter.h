ebc44ae53ced6e3d49150ec0233dad98f83d6635
Kittel-Watne <142875697+Kittel-Watne@users.noreply.github.com>
2024-04-02 16:48:24 +0200

    Creating Input in c++
    Created all neccesary parts needed for inputs in c++. Everything else can be done in Unreal Engine.

---
+++ Source/GameDevGrp10/Public/PlayerCharacter.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputComponent;
+struct FInputActionValue;
+class UInputAction;
+class UCameraComponent;
+
+UCLASS()
+class GAMEDEVGRP10_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+
+	/*
+	 * Attributes
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes")
+	USkeletalMeshComponent* PlayerMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes")
+	UCameraComponent* PlayerCamera;
+
+	/*
+	 * Inputs
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* IMC;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* Looking;
+
+	void Look(const FInputActionValue& Value);
+
+};
