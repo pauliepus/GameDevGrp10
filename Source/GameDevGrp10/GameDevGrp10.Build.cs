757583f128a0edf26dcd4d22547d9e2bd540b770
pauliepus <142883643+pauliepus@users.noreply.github.com>
2024-05-01 14:18:57 +0200

    Revert "Merge pull request #72 from pauliepus/Main_prototypeTest1"
    This reverts commit a524851c5e3c20ecc09038a1b503d2d943378fb2, reversing
    changes made to ddc34027c8b0c211ebece85745fc33b605c50216.

--- Source/GameDevGrp10/GameDevGrp10.Build.cs
+++ Source/GameDevGrp10/GameDevGrp10.Build.cs
@@ -8,10 +8,9 @@ public class GameDevGrp10 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","LevelSequence", "MovieScene" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
